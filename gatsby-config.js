a9a6502f7fbcb2df7c3848091bcf20d3f80ddd56
Ajit Singh <ajit.singh2905@gmail.com>
2019-01-08 08:30:57 +0530

    re-add purgecss

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,24 +13,38 @@ module.exports = {
         path: `${__dirname}/src/images`
       }
     },
+
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Ajit Singh`,
+        short_name: `Ajit Singh`,
         start_url: `/`,
-        background_color: `#ffcd72`,
+        background_color: `#fff`,
         theme_color: `#ffcd72`,
         display: `minimal-ui`,
-        icon: `src/images/logo.png` // This path is relative to the root of the site.
+        icon: `src/images/logo.png`
       }
     },
-    // ^ this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
+    // "gatsby-plugin-offline",
+
     "gatsby-plugin-postcss",
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true, // Print removed selectors and processed file names
+        // develop: true, // Enable while using `gatsby develop`
+        tailwind: true, // Enable tailwindcss support
+        purgeOnly: ["src/css/global.css"]
+        // whitelist: ['whitelist'], // Don't remove this selector
+        // ignore: ["/ignored.css", "prismjs/", "docsearch.js/"] // Ignore files/folders
+        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
+      }
+    },
+
     {
       resolve: `gatsby-transformer-remark`,
       options: {
@@ -39,12 +53,6 @@ module.exports = {
             resolve: `gatsby-remark-prismjs`,
             options: {
               classPrefix: "language-",
-              // This is used to allow setting a language for inline code
-              // (i.e. single backticks) by creating a separator.
-              // This separator is a string and will do no white-space
-              // stripping.
-              // A suggested value for English speakers is the non-ascii
-              // character '›'.
               inlineCodeMarker: null,
               showLineNumbers: false,
               noInlineHighlight: false
