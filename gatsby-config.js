8bb330d98cdb5b93bb0741a8c27aaa69de47e897
Ajit Singh <ajit.singh2905@gmail.com>
2019-01-08 09:28:07 +0530

    fix purging .injected-html issues

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
         printRejected: true, // Print removed selectors and processed file names
         // develop: true, // Enable while using `gatsby develop`
         tailwind: true, // Enable tailwindcss support
-        purgeOnly: ["src/css/global.css"]
+        purgeOnly: ["src/css/global.css"] // global.css contains Tailwind's @tailwind
         // whitelist: ['whitelist'], // Don't remove this selector
         // ignore: ["/ignored.css", "prismjs/", "docsearch.js/"] // Ignore files/folders
         // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
